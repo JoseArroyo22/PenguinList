9d9d4edf8307f713ded99fc2d069def11d0ee409
JoseArroyo22 <77021316+JoseArroyo22@users.noreply.github.com>
2021-06-15 12:26:36 -0700

    Initial commit

---
+++ src/PenguinList.java
@@ -0,0 +1,52 @@
+public class PenguinList {
+    class IntNode {
+        public int id, weight, height;
+        public IntNode next;
+
+        private IntNode(int i, int w, int h, IntNode n) {
+            id = i;
+            weight = w;
+            height = h;
+            next = n;
+        }
+
+    }
+
+    private IntNode first;
+    private int listSize;
+
+
+    public PenguinList(int i, int w, int h) {
+        first = new IntNode(i, w, h, null);
+    }
+
+    public void addFirst(int i, int w, int h) {
+        first = new IntNode(i, w, h, first);
+    }
+
+    public int getFirstID() {
+        return first.id;
+    }
+
+    public int getFirstHeight() {
+        return first.height;
+    }
+
+    public int getFirstWeight() {
+        return first.weight;
+    }
+
+    public int iterativeSize() {
+        IntNode p = first;
+        int totalSize = 0;
+        while (p != null) {
+            totalSize++;
+            p = p.next;
+            listSize++;
+        }
+        return totalSize;
+
+    }
+
+
+}
